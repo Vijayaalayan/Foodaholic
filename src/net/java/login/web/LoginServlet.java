efe282551c08fd48f06931c3d284ee1d801b61fa
Vijayaalayan A <54370583+Vijayaalayan@users.noreply.github.com>
2021-01-19 16:13:22 +0530

    Add files via upload

---
+++ src/net/java/login/web/LoginServlet.java
@@ -0,0 +1,53 @@
+package net.java.login.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.java.login.bean.LoginBean;
+import net.java.login.bean.database.LoginDao;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private LoginDao loginDao = new LoginDao();
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/foodielogin.jsp");
+		dispatcher.forward(request, response);
+	}
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        LoginBean loginBean = new LoginBean();
+        loginBean.setEmail(email);
+        loginBean.setPassword(password);
+
+        try {
+            if (loginDao.validate(loginBean)) {
+                //HttpSession session = request.getSession();
+                // session.setAttribute("username",username);
+                response.sendRedirect("foodiesuccess.jsp");
+            } else {
+//                HttpSession session = request.getSession();
+                //session.setAttribute("user", username);
+                response.sendRedirect("foodielogin.jsp");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/net/java/registration/controller/FoodieServlet.java
@@ -0,0 +1,73 @@
+package net.java.registration.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.java.registration.dao.FoodieDao;
+import net.java.registration.model.Foodie;
+
+/**
+ * Servlet implementation class FoodieServlet
+ */
+@WebServlet("/register")
+public class FoodieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private FoodieDao foodieDao = new FoodieDao();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FoodieServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/foodieregister.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String number = request.getParameter("number");
+		String place = request.getParameter("place");
+		String password = request.getParameter("password");
+		String guide = request.getParameter("guide");
+		
+		Foodie foodie = new Foodie();
+		foodie.setName(name);
+		foodie.setEmail(email);
+		foodie.setNumber(number);
+		foodie.setPlace(place);
+		foodie.setPassword(password);
+		foodie.setGuide(guide);
+		
+		try {
+			foodieDao.registerFoodie(foodie);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/foodiedetails.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
